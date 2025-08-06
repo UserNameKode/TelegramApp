6a463bacaaee7c1742d157d36bf3e914181d1b43
UserNameKode <timofejtimakov355@gmail.com>
2025-08-06 10:45:05 +0400

    Add files via upload

--- data.js
+++ data.js
@@ -5,32 +5,37 @@ class DataService {
             {
                 id: 'engine',
                 title: 'Двигатель и компоненты',
-                image: 'https://i.ibb.co/M6QMKNP/engine.jpg',
-                count: 5
+                image: 'https://i.ibb.co/9T4Vv0m/engine.jpg',
+                count: 5,
+                description: 'Двигатели, поршни, клапаны и другие компоненты'
             },
             {
                 id: 'brake',
                 title: 'Тормозная система',
-                image: 'https://i.ibb.co/4WM5tJX/brake.jpg',
-                count: 3
+                image: 'https://i.ibb.co/1RW9ykw/brake.jpg',
+                count: 3,
+                description: 'Тормозные диски, колодки, суппорты'
             },
             {
                 id: 'suspension',
                 title: 'Подвеска и ходовая',
-                image: 'https://i.ibb.co/tQc6pKq/suspension.jpg',
-                count: 4
+                image: 'https://i.ibb.co/0M8yh7c/suspension.jpg',
+                count: 4,
+                description: 'Амортизаторы, пружины, рычаги'
             },
             {
                 id: 'electrical',
                 title: 'Электрооборудование',
-                image: 'https://i.ibb.co/VvCnMxp/electrical.jpg',
-                count: 3
+                image: 'https://i.ibb.co/vw8YM0L/electrical.jpg',
+                count: 3,
+                description: 'Генераторы, стартеры, датчики'
             },
             {
                 id: 'body',
                 title: 'Кузовные детали',
-                image: 'https://i.ibb.co/C2z0KSJ/body.jpg',
-                count: 2
+                image: 'https://i.ibb.co/Kj3kW9v/body.jpg',
+                count: 2,
+                description: 'Капоты, крылья, бамперы'
             }
         ];
 
@@ -41,24 +46,30 @@ class DataService {
                 categoryId: 'engine',
                 title: 'Поршневая группа ВАЗ 2110',
                 price: 12500,
-                image: 'https://i.ibb.co/HdKXzKQ/piston-group.jpg',
-                description: 'Комплект поршневой группы для ВАЗ 2110. Включает поршни, кольца и пальцы.'
+                image: 'https://i.ibb.co/3WJQd1H/vaz-piston.jpg',
+                description: 'Комплект поршневой группы для ВАЗ 2110. Включает поршни, кольца и пальцы.',
+                manufacturer: 'АвтоВАЗ',
+                article: 'PG-2110-001'
             },
             {
                 id: 'engine-2',
                 categoryId: 'engine',
                 title: 'Комплект ГРМ Ford Focus',
                 price: 8900,
-                image: 'https://i.ibb.co/0MZ0H5Y/timing-belt.jpg',
-                description: 'Комплект ГРМ для Ford Focus 2.0. Ремень, ролики, натяжитель.'
+                image: 'https://i.ibb.co/XxTNGxk/ford-timing.jpg',
+                description: 'Комплект ГРМ для Ford Focus 2.0. Ремень, ролики, натяжитель.',
+                manufacturer: 'Gates',
+                article: 'K015603XS'
             },
             {
                 id: 'engine-3',
                 categoryId: 'engine',
                 title: 'Масляный насос Toyota',
                 price: 5600,
-                image: 'https://i.ibb.co/XS8DNzp/oil-pump.jpg',
-                description: 'Масляный насос для Toyota Camry 2.4. Оригинал.'
+                image: 'https://i.ibb.co/Lx6ztyL/toyota-oil.jpg',
+                description: 'Масляный насос для Toyota Camry 2.4. Оригинал.',
+                manufacturer: 'Toyota',
+                article: '15100-28020'
             },
 
             // Тормоза
@@ -67,16 +78,20 @@ class DataService {
                 categoryId: 'brake',
                 title: 'Дисковые тормоза Brembo',
                 price: 15800,
-                image: 'https://i.ibb.co/VqFgRf7/brembo-brakes.jpg',
-                description: 'Комплект передних тормозных дисков Brembo. Высокая производительность.'
+                image: 'https://i.ibb.co/GCwPnxP/brembo.jpg',
+                description: 'Комплект передних тормозных дисков Brembo. Высокая производительность.',
+                manufacturer: 'Brembo',
+                article: '09.C881.13'
             },
             {
                 id: 'brake-2',
                 categoryId: 'brake',
                 title: 'Колодки Ferodo BMW',
                 price: 4500,
-                image: 'https://i.ibb.co/0MKyqJ6/brake-pads.jpg',
-                description: 'Передние тормозные колодки Ferodo для BMW 3 серии.'
+                image: 'https://i.ibb.co/W2zqBgL/ferodo.jpg',
+                description: 'Передние тормозные колодки Ferodo для BMW 3 серии.',
+                manufacturer: 'Ferodo',
+                article: 'FDB4765'
             },
 
             // Подвеска
@@ -85,16 +100,20 @@ class DataService {
                 categoryId: 'suspension',
                 title: 'Амортизаторы KYB',
                 price: 7800,
-                image: 'https://i.ibb.co/C1Q8Jyq/shock-absorber.jpg',
-                description: 'Передние амортизаторы KYB для Honda Civic. Комплект 2 шт.'
+                image: 'https://i.ibb.co/0mbL3Qp/kyb.jpg',
+                description: 'Передние амортизаторы KYB для Honda Civic. Комплект 2 шт.',
+                manufacturer: 'KYB',
+                article: '334841'
             },
             {
                 id: 'suspension-2',
                 categoryId: 'suspension',
                 title: 'Рычаги Audi',
                 price: 12300,
-                image: 'https://i.ibb.co/xGV2Yyq/control-arm.jpg',
-                description: 'Комплект передних рычагов для Audi A4 B8.'
+                image: 'https://i.ibb.co/C1j7tYd/audi-arm.jpg',
+                description: 'Комплект передних рычагов для Audi A4 B8.',
+                manufacturer: 'Lemförder',
+                article: '3463202'
             },
 
             // Электрика
@@ -103,16 +122,20 @@ class DataService {
                 categoryId: 'electrical',
                 title: 'Генератор Bosch',
                 price: 18500,
-                image: 'https://i.ibb.co/0XKQ8LS/generator.jpg',
-                description: 'Генератор Bosch для Volkswagen Polo 1.6.'
+                image: 'https://i.ibb.co/9gGGfRz/bosch-gen.jpg',
+                description: 'Генератор Bosch для Volkswagen Polo 1.6.',
+                manufacturer: 'Bosch',
+                article: '0124525087'
             },
             {
                 id: 'electrical-2',
                 categoryId: 'electrical',
                 title: 'Стартер Valeo',
                 price: 14200,
-                image: 'https://i.ibb.co/wSxVKqc/starter.jpg',
-                description: 'Стартер Valeo для Renault Logan. Новый.'
+                image: 'https://i.ibb.co/xS8zfCN/valeo-starter.jpg',
+                description: 'Стартер Valeo для Renault Logan. Новый.',
+                manufacturer: 'Valeo',
+                article: '458204'
             },
 
             // Кузов
@@ -121,16 +144,20 @@ class DataService {
                 categoryId: 'body',
                 title: 'Капот Hyundai Solaris',
                 price: 21500,
-                image: 'https://i.ibb.co/0QKwVJy/hood.jpg',
-                description: 'Капот для Hyundai Solaris 2017+. Оригинал.'
+                image: 'https://i.ibb.co/mCgbb7h/solaris-hood.jpg',
+                description: 'Капот для Hyundai Solaris 2017+. Оригинал.',
+                manufacturer: 'Hyundai',
+                article: '66400-H5000'
             },
             {
                 id: 'body-2',
                 categoryId: 'body',
                 title: 'Крылья Toyota Corolla',
                 price: 8900,
-                image: 'https://i.ibb.co/XxQ3p1d/fender.jpg',
-                description: 'Переднее крыло для Toyota Corolla 2019+. Левое и правое.'
+                image: 'https://i.ibb.co/0XZvLk6/corolla-fender.jpg',
+                description: 'Переднее крыло для Toyota Corolla 2019+. Левое и правое.',
+                manufacturer: 'Toyota',
+                article: '53801-02A30'
             }
         ];
     }
